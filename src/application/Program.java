51f6054b3e57a1347f6ae6bd893c2920833d7585
diotec-curso <dioluis@pop-os.localdomain>
2021-07-22 09:36:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,11 +12,15 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "Books");
-		System.out.println(department);
+		
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 		
+			
 	}
 
 }
