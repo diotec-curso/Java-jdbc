a1af9234cd47b1e284819052c43e81a090ce24d5
diotec-curso <dioluis@pop-os.localdomain>
2021-07-22 23:43:21 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,7 +32,14 @@ public class Program {
 			System.out.println(obj);
 
 		}
+		System.out.println();
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller seller2 = new Seller(null, "Greg Black", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(seller2);
+		System.out.println("Inserted! New id = " + seller2.getId());
+
+		}
 
 	}
 
-}
+
